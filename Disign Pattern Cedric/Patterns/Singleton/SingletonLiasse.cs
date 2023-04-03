d0c7b84b1ffcbd80f651113accd5811b848c990e
Vincent REMY <vincentremy222@gmail.com>
2023-04-03 09:53:48 +0200

    feat : singleton pattern

---
+++ Disign Pattern Cedric/Patterns/Singleton/SingletonLiasse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Singleton
+{
+    class SingletonLiasse
+    {
+        private SingletonLiasse() { }
+
+        private static SingletonLiasse instance = null;
+        private List<string> liasse = new List<string>();
+
+        public static SingletonLiasse getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new SingletonLiasse();
+            }
+            return instance;
+        }
+
+        public void Add(string doc)
+        {
+            liasse.Add(doc);
+        }
+
+        public void Remove(string doc)
+        {
+            liasse.Remove(doc);
+        }
+
+        public List<string> GetLiasse()
+        {
+            return liasse;
+        }
+    }
+}
---
+++ Disign Pattern Cedric/Program.cs
@@ -0,0 +1,12 @@
+﻿using Patterns.Singleton;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("test singleton");
+        SingletonLiasse.getInstance().Add("doc1");
+        SingletonLiasse.getInstance().Remove("doc1");
+        Console.WriteLine("OK");
+    }  
+}
